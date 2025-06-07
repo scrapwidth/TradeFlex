a5642264bcef142e8e9797fee52465fc2b0be5cc
scrapwidth <134894399+scrapwidth@users.noreply.github.com>
2025-06-06 18:49:13 -0600

    Refactor AlgorithmRunner to use generic instantiation

---
+++ TradeFlex.Tests/AlgorithmRunnerTests.cs
@@ -0,0 +1,24 @@
+using TradeFlex.Abstractions;
+using TradeFlex.Core;
+using TradeFlex.SampleStrategies;
+
+namespace TradeFlex.Tests;
+
+public class AlgorithmRunnerTests
+{
+    [Fact]
+    public void AlgorithmRunnerRunsSmaSample()
+    {
+        var bars = new List<Bar>
+        {
+            new(DateTime.UtcNow, 1, 1, 1, 1, 100),
+            new(DateTime.UtcNow, 2, 2, 2, 2, 100),
+            new(DateTime.UtcNow, 3, 3, 3, 3, 100),
+            new(DateTime.UtcNow, 4, 4, 4, 4, 100),
+        };
+
+        var trades = AlgorithmRunner.Run<SimpleSmaCrossoverAlgorithm>(bars, 2, 3);
+
+        Assert.NotNull(trades);
+    }
+}
